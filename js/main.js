6574bd017317ce11fb0310bfa5255420d5ab0580
Brantley English <brantley@sodiumhalogen.com>
2018-07-30 22:05:22 -0500

    📦  NEW: adds new images

--- js/main.js
+++ js/main.js
@@ -1,12 +1,108 @@
 $(document).ready(function () {
-  $('.slicklawncare').slick({
+  $('.slicklawncare1').slick({
     arrows: true,
     infinite: false,
     speed: 300,
     slidesToShow: 4,
     slidesToScroll: 4,
-    prevArrow: $('.prev'),
-    nextArrow: $('.next'),
+    prevArrow: $('.prev1'),
+    nextArrow: $('.next1'),
+    responsive: [{
+        breakpoint: 1500,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: false
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+  $('.slicklawncare2').slick({
+    arrows: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    prevArrow: $('.prev2'),
+    nextArrow: $('.next2'),
+    responsive: [{
+        breakpoint: 1500,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: false
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+  $('.slicklawncare3').slick({
+    arrows: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    prevArrow: $('.prev3'),
+    nextArrow: $('.next3'),
+    responsive: [{
+        breakpoint: 1500,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: false
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+  $('.slicklawncare4').slick({
+    arrows: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    prevArrow: $('.prev4'),
+    nextArrow: $('.next4'),
     responsive: [{
         breakpoint: 1500,
         settings: {
